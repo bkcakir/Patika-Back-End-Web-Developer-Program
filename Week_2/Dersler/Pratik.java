c8037d0af2c3bc77ec4506fca4efdaaec282206b
Burak Kaan Çakır <bk.cakir@outlook.com>
2025-03-13 20:02:02 +0300

    Yeni pratikler ve 2. Hafta Ders İçeriği eklendi - 2. Hafta Dersleri (Java 101) eklendi   - Method tanımlama, Recursive methods,Method Pratikleri   - Recursive Method Pratikleri - Ödevler eklendi   - Asal Sayı Bulan Program

---
+++ Week_2/Dersler/Pratik.java
@@ -0,0 +1,36 @@
+package Week_2.Dersler;
+
+public class Pratik {
+    public static void main(String[] args) {
+
+        System.out.println("Factorial : "+Factorial(5));
+        System.out.println("Fibonacci : "+Fibonacci(9));
+    }
+
+    public  static  int Factorial(int number)
+    {
+        if(number==0)
+        {
+            return 1;
+        }
+        else
+        {
+            return number * (Factorial(number-1));
+        }
+
+    }
+
+    private static int Fibonacci(int Number)
+    {
+        if(Number==0)
+        {
+            return 0;
+        }
+        else if (Number==1) {
+            return 1;
+        }
+        else {
+            return Fibonacci(Number - 1) + Fibonacci(Number - 2);
+        }
+    }
+}
---
+++ Week_2/Primenumber.java
@@ -0,0 +1,34 @@
+package Week_2;
+
+import java.util.Scanner;
+
+public class Primenumber {
+    static boolean AsalSayi(int A,int i)
+    {
+        if (A <= 2) {
+            return (A == 2);
+        }
+        if (A % i == 0) {
+            return false;
+        }
+        if (i * i > A) {
+            return true;
+        }
+        return AsalSayi(A, i + 1);
+    }
+
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        System.out.print("Bir sayı giriniz: ");
+        int sayi = scanner.nextInt();
+
+        if (AsalSayi(sayi,2 )) {
+            System.out.println(sayi + " bir asal sayıdır.");
+        } else {
+            System.out.println(sayi + " bir asal sayı değildir.");
+        }
+
+        scanner.close();
+    }
+}
